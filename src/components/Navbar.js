97c1e1a77c03a19d11194bcdb0e1df37b7da7673
philiptwcn <philiptwcn@gmail.com>
2019-08-04 01:01:09 +0800

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,14 +35,14 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar_is-transparent"
+        className="navbar is-transparent"
         role="navigation"
         aria-label="main-navigation"
       >
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="harris" style={{ width: '150px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
