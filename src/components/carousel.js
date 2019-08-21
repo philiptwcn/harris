82ef1f039f5c85c1fb99c93aa1845ae930b5e957
philiptwcn <philiptwcn@gmail.com>
2019-08-21 18:30:53 +0800

    11

---
+++ src/components/carousel.js
@@ -0,0 +1,12 @@
+import bulmaCarousel from '~bulma-carousel/dist/js/bulma-carousel.min.js'
+
+// Initialize all elements with carousel class.
+const carousels = bulmaCarousel.attach('.carousel', options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+}
+
+export default carousels
