886937da83fdc28001e9c1de62158394326d3f1d
philiptwcn <philiptwcn@gmail.com>
2019-08-21 16:04:08 +0800

    10

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Harris - Asia',
+    title: 'Harris Asia',
     description:
       'Aristocratic fashion of Europe, creatively originated for the public.',
   },
